abab3dbc1e4c1990c870bd1742e7c41b856d23ad
JoEDKLol <wibterflower@gmail.com>
2024-03-08 21:10:07 -0800

    ESLint Extention

---
+++ src/app/components/layout/Index.tsx
@@ -0,0 +1,12 @@
+const LayoutIndex = ({ children }: { children: React.ReactNode }) => {
+
+  function test(){
+    console.log('1' + 1)
+  }
+
+  return (<div>
+    최초실행되는 페이지입니다.
+  </div>)
+};
+
+export default LayoutIndex;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import LayoutIndex from "./components/layout/Index";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+          <LayoutIndex>{children}</LayoutIndex>
+        </body>
     </html>
   );
 }
